62b7243830c6b61fdf2f5bca4f985a6483b8c4cc
SifEddine05 <sifousamir72@gmail.com>
2023-04-29 00:35:53 +0100

    adding the login form in front end

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,6 @@ import Forgotpwd from "./pages/Forgotpwd";
 
 
 
-
 function App() {
   return (
    
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,9 +1,23 @@
+import { useState } from 'react';
 import login from '../assets/Login.jpg'
 import { Link } from  "react-router-dom";
 
 
 
 const Login = () => {
+    const [error,setError] = useState(null)
+    const [email,setEmail]=useState('')
+    const [password,setPassword]=useState('')
+    const submit =()=>{
+        setError(null)
+        if(email==='' || password==='')
+        {
+            setError('Please fill all the fields')
+        }
+        else{
+
+        }
+    }
     return ( 
     <div className="bg-[#29abe2] bg-opacity-30 lg:p-14 md:p-10 sm:p-6 p-4 ">
         <div className=' mb-[5%] flex justify-between bg-[#FFFF] mx-[5%] rounded-xl lg:p-5 md:p-4 sm:p-3 p-2 shadow-lg'>
@@ -15,15 +29,16 @@ const Login = () => {
                 <br   />
 
                 <label className="lg:text-[18px]  md:text-[15px] sm:text-[12px] text-[9px] font-medium "> Email <br/>
-                    <input type="email" required placeholder="john.doe@company.com" maxLength='50'  className="bg-gray-50 w-full border-2 shadow-lg border-gray-300 text-gray-900 lg:text-[16px] md:text-[13px] sm:text-[10px] text-[7px] rounded-lg focus:ring-blue-500 focus:border-blue-500 block  md:p-2.5 p-1 "/>
+                    <input type="email" value={email} onChange={(e)=>{setEmail(e.target.value)}} required placeholder="john.doe@company.com" maxLength='50'  className="bg-gray-50 w-full border-2 shadow-lg border-gray-300 text-gray-900 lg:text-[16px] md:text-[13px] sm:text-[10px] text-[7px] rounded-lg focus:ring-blue-500 focus:border-blue-500 block  md:p-2.5 p-1 "/>
                 </label>
                 <br   />
                 <label className="lg:text-[18px]  md:text-[15px] sm:text-[12px] text-[9px] font-medium "> Password 
                     <Link to='/frgpass' className='  lg:text-[14px] md:text-[12px] sm:text-[9px] text-[7px] underline ml-[20%] text-[#293be2]'>Forgot Password?</Link>
                     <br/>    
-                    <input type="password" required placeholder="Password" maxLength='50' minLength='6' className="bg-gray-50  w-full border-2 shadow-lg border-gray-300 text-gray-900 lg:text-[16px] md:text-[13px] sm:text-[10px] text-[7px] rounded-lg focus:ring-blue-500 focus:border-blue-500 block  md:p-2.5 p-1 "/>
+                    <input type="password" value={password} onChange={(e)=>{setPassword(e.target.value)}} required placeholder="Password" maxLength='50' minLength='6' className="bg-gray-50  w-full border-2 shadow-lg border-gray-300 text-gray-900 lg:text-[16px] md:text-[13px] sm:text-[10px] text-[7px] rounded-lg focus:ring-blue-500 focus:border-blue-500 block  md:p-2.5 p-1 "/>
                 </label>
-                <button className="  border-2 shadow-lg border-gray-300 text-gray-900 bg-[#29abe2] hover:cursor-pointer hover:text-white lg:text-[16px] font-semibold md:text-[13px] sm:text-[10px] text-[7px] rounded-lg focus:ring-blue-500 focus:border-blue-500    md:p-2.5 p-1  block w-[50%] mt-[10%] ml-[12%]"  >Login</button>
+                {error && <h3 className="mt-4 md:text-lg text-sm text-red-600 font-bold mb-4 ml-4">{error} </h3> }
+                <button className="  border-2 shadow-lg border-gray-300 text-gray-900 bg-[#29abe2] hover:cursor-pointer hover:text-white lg:text-[16px] font-semibold md:text-[13px] sm:text-[10px] text-[7px] rounded-lg focus:ring-blue-500 focus:border-blue-500    md:p-2.5 p-1  block w-[50%] mt-[10%] ml-[12%]"  onClick={submit}>Login</button>
                 <hr className='mt-[5%] mb-[5%] w-[75%]'/> 
             </div>
             <img src={login} alt='login' className=' w-[60%] rounded-lg  '/>
